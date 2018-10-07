51fa8eaa753eed9acf150e2e18daec94ae0f1d75
Gopinath Surendran <gsurendran003@ca.nam.ad.pwcinternal.com>
2018-10-06 23:58:22 -0400

    added default routing from server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import debug from "debug";
 import morgan from "morgan";
 import chalk from "chalk";
+import path from "path";
 
 const app = express();
 const port: any = process.env.PORT || 5000;
@@ -11,5 +12,14 @@ app.use(morgan('tiny'));
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express of Shoping site' });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => debug(`Listening  on port ${chalk.green(port)}`));
