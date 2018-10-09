76eb9d683217e98c37cd713e2744781b66658ec1
sangeeth.s <sangeeth.s@impelsys.com>
2018-10-09 19:48:05 +0530

    segregate app and server

---
+++ src/dbConnection.ts
@@ -0,0 +1,13 @@
+/*
+import {mongoclient} from "mongodb";
+import express from "express";
+
+const app = express();
+
+app.get('/db',async(req,res) =>{
+    client = await MongoClient.connect(url);
+    database = client.db("shoping-site");
+    console.log("connected to mongo");
+});
+
+*/
