37e0d4872af9bae0b10f0d1be24fae760aee9218
Gopinath Surendran <gsurendran003@ca.nam.ad.pwcinternal.com>
2018-10-08 23:23:06 -0400

    Testing mongo db connection

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,47 @@ import morgan from "morgan";
 import chalk from "chalk";
 import path from "path";
 
+// temp testing
+import { MongoClient } from "mongodb";
+
 const app = express();
 const port: any = process.env.PORT || 5000;
 
 app.use(morgan('tiny'));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express of Shoping site, auto deployment enabled.' });
+
+app.get('/api/product', (req, res) => {
+    const url = "mongodb://admin:Shopingsite123@ds027748.mlab.com:27748/shoping-site";
+    console.log('product');
+    (async () => {
+        let client;
+        try {
+            client = await MongoClient.connect(url);
+            console.log("connected to mongo");
+            const db = client.db("shoping-site");
+            const response = await db.collection("product").find().toArray();
+            res.json(response);
+        } catch (err) {
+            res.send("error:" + JSON.stringify(err));
+        }
+    })();
+});
+
+app.get('/api/putproduct', (req, res) => {
+    const url = "mongodb://admin:Shopingsite123@ds027748.mlab.com:27748/shoping-site";
+    console.log('product');
+    (async () => {
+        let client;
+        try {
+            client = await MongoClient.connect(url);
+            console.log("connected to mongo");
+            const db = client.db("shoping-site");
+            const response = await db.collection("product").insertOne({ name: 'product-' + (Math.random() * 100) });
+            res.json(response);
+        } catch (err) {
+            res.send("error:" + JSON.stringify(err));
+        }
+    })();
 });
 
 if (process.env.NODE_ENV === 'production') {
