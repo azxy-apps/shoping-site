004b73fb2c036ca7e7291b9ac9ef9ad1ddf15c9c
Gopinath Surendran <gsurendran003@ca.nam.ad.pwcinternal.com>
2018-10-15 20:08:29 -0400

    splitting server and client

--- src/route.ts
+++ src/route.ts
@@ -5,4 +5,7 @@ import productController from './controller/productController';
 module.exports = (app: any) => {
     app.use('/api/products', productController);
     // app.use('/user', require('./controller/userController'));
+    app.use('/', (req, res) => {
+        res.json('Site is running!');
+    });
 };
